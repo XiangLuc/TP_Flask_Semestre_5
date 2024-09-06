3f872a78e3380de794b618b603ec91d793114766
XiangLuc <xiangluc01@gmail.com>
2024-09-06 11:40:23 +0200

    création de fonction pour récupérer la liste des taches et une tache par uuid

--- task_db.py
+++ task_db.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 '''
 Une tâche contient les attributs suivants :
@@ -24,3 +24,13 @@ task_db est un index qui se présente sous la forme suivante :
 task_bp = Blueprint('task_db', __name__)
 
 task_db = dict()
+
+
+@task_bp.route('/', methods=['GET'])
+def get_tasks():
+    return jsonify(task_db)
+
+
+@task_bp.route('/:id', methods=['GET'])
+def get_task_by_id(id):
+    return jsonify(task_db.get(id))
