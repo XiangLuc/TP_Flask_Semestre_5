b5169a74ad2855a1d42ff9eb5b19afc79e4bbddd
XiangLuc <xiangluc01@gmail.com>
2024-09-06 11:16:13 +0200

    init commit

---
+++ task_db.py
@@ -0,0 +1,21 @@
+'''
+Une tâche contient les attributs suivants :
+task_id : uuid
+name : string
+description : string
+status : string
+created_at : str
+updated_at : str
+task_db est un index qui se présente sous la forme suivante :
+{
+    "uuid1" : {
+        "task_id" : "uuid1",
+        "name": "Nom",
+        "description": "Desc",
+        "status": "TODO",
+        "created_at" : "date de création",
+        "updated_at" : "date de mise à jour"
+    }
+}
+'''
+task_db = dict()
